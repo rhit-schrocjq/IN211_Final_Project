3ec70e78f3284d2008d68800adc5b34f30951064
rhit-mirandm1 <95196506+rhit-mirandm1@users.noreply.github.com>
2023-10-20 12:35:02 -0400

    first two pages

---
+++ public/scripts/main.js
@@ -0,0 +1,5 @@
+document.getElementById("bottomText").addEventListener("click", myFunction);
+
+function myFunction() {
+  document.getElementById("mapAll").scrollIntoView();
+}
---
+++ public/scripts/mainResources.js
@@ -0,0 +1,12 @@
+
+$('.carousel').carousel()
+
+
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
