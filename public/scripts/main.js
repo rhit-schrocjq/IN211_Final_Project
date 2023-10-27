8940370c6158780c9461fd05ec78d5f7d6f7aff4
ROSE-HULMAN\schrocjq <schrocjq@rose-hulman.edu>
2023-10-27 13:42:47 -0400

    html and css updates

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,11 +1,4 @@
 
-function myFunction() {
-  document.getElementById("mapAll").scrollIntoView();
-}
-
-
-
-
 main = function () {
   document.querySelector("#first").onclick = (event) => {
     window.open('index.html')
@@ -16,6 +9,12 @@ main = function () {
   document.querySelector("#third").onclick = (event) => {
     window.open('template.html');
   }
+  document.getElementById("LAKE").onclick = (event) => {
+    window.open('resources.html')
+  }
+  document.getElementById("bottomText").onclick = (event) => {
+    document.getElementById("indianaMap").scrollIntoView();
+  };
 
   
 }
