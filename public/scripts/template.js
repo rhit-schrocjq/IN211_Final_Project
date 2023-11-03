96fe30ed67fd3a7cff0dbf884c64774fef03df3d
ROSE-HULMAN\schrocjq <schrocjq@rose-hulman.edu>
2023-11-03 17:15:02 -0400

    individual resources page working

--- public/scripts/template.js
+++ public/scripts/template.js
@@ -1,16 +1,54 @@
-main = function () {
-    document.querySelector("#first").onclick = (event) => {
-      window.open('index.html')
+let id="";
+
+async function loadData() {
+  try {
+    const response = await fetch(`http://localhost:3030/resources/${id}`);
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
     }
-    document.querySelector("#second").onclick = (event) => {
-      window.open('resources.html');
+    const data = await response.json();
+    console.log(data);
+    document.getElementById('Status').innerHTML = data.status;
+    if (data.status == 'Active') {
+      document.getElementById('Status').style.color = "#00C000";
+    } else {
+      document.getElementById('Status').style.color = "#C00000";
     }
-    document.querySelector("#third").onclick = (event) => {
-      window.open('template.html');
-    }
+    document.getElementById('Resource').innerHTML = data.agency_name;
+    document.getElementById('County').innerHTML = data.county;
+    document.getElementById('ResourceType').innerHTML = data.taxonomy_name;
+    document.getElementById('DateUpdated').innerHTML = data.lastupdated ? data.lastupdated : data.createdon;
+    document.getElementById('Website').innerHTML = data.service_website;
+    document.getElementById('Website').href = data.service_website;
+    document.getElementById('Description').innerHTML = data.service_description;
+    document.getElementById('Eligibility').innerHTML = data.site_eligibility;
+    document.getElementById('ApplicationProcess').innerHTML = data.site_details;
+    document.getElementById('ContactInfo').innerHTML = data.service_email + "<br>" + data.site_number;
+    document.getElementById('Schedule').innerHTML = data.site_schedule;
+    document.getElementById('Address').src = "https://www.google.com/maps/embed/v1/directions?key=AIzaSyAnCzeGnuCVofrTrSSVKSbm1VLKXPuSJRQ&origin=City+Hall,Indiana,IN&destination=" + data.latitude + "," + data.longitude + "&avoid=tolls|highways";
+
+  } catch (error) {
+    console.error('Error fetching books:', error);
   }
-  
-  
-  
-  
-  main();
+};
+
+main = function () {
+  document.querySelector("#first").onclick = (event) => {
+    window.open('index.html')
+  }
+  document.querySelector("#second").onclick = (event) => {
+    window.open('resources.html');
+  }
+  document.querySelector("#third").onclick = (event) => {
+    window.open('template.html');
+  }
+  param = window.location.search.split("=")
+  if (param[0] == "?id") {
+    id = param[1];
+  }
+  loadData();
+}
+
+
+
+main();
--- server.js
+++ server.js
@@ -5,23 +5,21 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-const dataFilePath = './data/data.json';
+const resourcesFilePath = './data/resources.json';
+
+function SortByName(a, b){
+    var aName = a.agency_name.toLowerCase();
+    var bName = b.agency_name.toLowerCase(); 
+    return ((aName < bName) ? -1 : ((aName > bName) ? 1 : 0));
+}
 
 async function readData() {
     try {
-        const data = await fs.readFile(dataFilePath, 'utf-8');
-        return JSON.parse(data);
+        const resources = await fs.readFile(resourcesFilePath, 'utf-8');
+        return JSON.parse(resources);
     } catch (error) {
         console.error('Error reading data:', error);
         return [];
-    }
-}
-
-async function writeData(data) {
-    try {
-        await fs.writeFile(dataFilePath, JSON.stringify(data, null, 2), 'utf-8');
-    } catch (error) {
-        console.error('Error writing data:', error);
     }
 }
 
@@ -30,57 +28,120 @@ app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
 });
 
-app.get('/data', async (req,res) => {
-    const data = await readData();
-    res.json(data);
+app.get('/resources', async (req,res) => {
+    const resources = await readData();
+    res.json(resources);
 })
 
-app.get('/data/:county', async (req,res) => {
+app.get('/resources/all/:selection', async (req,res) => {
+    const {selection} = req.params;
+    let resources = await readData();
+    if (!resources) {
+        return res.status(404).json({error: 'Data not found'});
+    }
+    selectionArray = selection.split('-');
+    if (selectionArray.length === 1) {
+        resources = resources[selectionArray[0]];
+    }
+    else if (selectionArray.length === 2) {
+        resources = resources.slice(selectionArray[0], selectionArray[1]);
+    } 
+    res.json(resources);
+})
+
+app.get('/resources/all/:order/:selection', async (req,res) => {
+    const {order, selection} = req.params;
+    let resources = await readData();
+    if (!resources) {
+        return res.status(404).json({error: 'Data not found'});
+    }
+    if (order == "A") {
+        resources.sort(SortByName);
+    }
+    selectionArray = selection.split('-');
+    if (selectionArray.length === 1) {
+        resources = resources[selectionArray[0]];
+    }
+    else if (selectionArray.length === 2) {
+        resources = resources.slice(selectionArray[0], selectionArray[1]);
+    } 
+    res.json(resources);
+})
+
+app.get('/resources/county/:county', async (req,res) => {
     const {county} = req.params;
-    const datas = await readData();
-    const data = datas.find(data => data.county === county);
-    if (!data) {
+    const resources = await readData();
+    const resourcesFiltered = resources.filter(resource => resource.county === county);
+    if (!resourcesFiltered) {
         return res.status(404).json({error: 'Data not found'});
     }
-    res.json(data);
+    res.json(resourcesFiltered);
 })
 
-app.post('/data', async (req, res) => {
-    const {title, author} = req.body;
-    const datas = await readData();
-    const newID = datas.length > 0 ? Math.max(...datas.map(data => data.id)) + 1 : 1;
-    const newData = { id: newID, title, author };
-    datas.push(newData);
-    await writeBooks(datas);
-    res.status(201).json(newData);
-});
-
-app.put('/data/:id', async (req, res) => {
-    const {title, author} = req.body;
-    const {id} = req.params;
-    const datas = await readData();
-    const data = datas.find(data => data.id === parseInt(id, 10));
-    if (!data) {
+app.get('/resources/county/:county/:order', async (req,res) => {
+    const {county, order} = req.params;
+    const resources = await readData();
+    const resourcesFiltered = resources.filter(resource => resource.county === county);
+    if (!resourcesFiltered) {
         return res.status(404).json({error: 'Data not found'});
     }
-    data.title = title || data.title;
-    data.author = author || data.author;
+    if (order == "A") {
+        resourcesFiltered.sort(SortByName);
+    }
+    res.json(resourcesFiltered);
+})
 
-    await writeData(datas);
-    res.json(datas[dataIndex]);
-});
-
-app.delete('/data/:id', async (req, res) => {
-    const {id} = req.params;
-    const datas = await readData();
-    const initialLength = datas.length;
-    const remainingDatas = datas.filter(data => data.id !== parseInt(id, 10));
-
-    if (remainingDatas.Length === initialLength) {
+app.get('/resources/county/:county/:order/:selection', async (req,res) => {
+    const {county, order, selection} = req.params;
+    const resources = await readData();
+    let resourcesFiltered = resources.filter(resource => resource.county === county);
+    if (!resourcesFiltered) {
         return res.status(404).json({error: 'Data not found'});
     }
-    await writeData(remainingDatas);
-    res.json({message: 'Data successfully deleted'});
-});
+    if (order == "A") {
+        resourcesFiltered.sort(SortByName);
+    }
+    selectionArray = selection.split('-');
+    if (selectionArray.length === 1) {
+        resourcesFiltered = resourcesFiltered[selectionArray[0]];
+    }
+    else if (selectionArray.length === 2) {
+        resourcesFiltered = resourcesFiltered.slice(selectionArray[0], selectionArray[1]);
+    } 
+    res.json(resourcesFiltered);
+})
+
+app.get('/resources/county/:county/all/:selection', async (req,res) => {
+    const {county, selection} = req.params;
+    const resources = await readData();
+    let resourcesFiltered = resources.filter(resource => resource.county === county);
+    if (!resourcesFiltered) {
+        return res.status(404).json({error: 'Data not found'});
+    }
+    selectionArray = selection.split('-');
+    if (selectionArray.length === 1) {
+        resourcesFiltered = resourcesFiltered[selectionArray[0]];
+    }
+    else if (selectionArray.length === 2) {
+        resourcesFiltered = resourcesFiltered.slice(selectionArray[0], selectionArray[1]);
+    } 
+    res.json(resourcesFiltered);
+})
+
+app.get('/resources/:taxonomy_code', async (req,res) => {
+    const {taxonomy_code} = req.params;
+    const params = taxonomy_code.split('+');
+    const resources = await readData();
+    const taxonomyFilter = resources.filter(resource => resource.taxonomy_code == params[0]);
+    const agencyIdFilter = taxonomyFilter.filter(resource => resource.agency_id == params[1]);
+    const siteIdFilter = agencyIdFilter.filter(resource => resource.site_id == params[2]);
+    if (!siteIdFilter) {
+        return res.status(404).json({error: 'Data not found'});
+    }
+    if(siteIdFilter.length>1) {
+        return res.status(404).json({error: 'Not Singular Data'});
+    }
+    res.json(siteIdFilter[0]);
+})
 
 
