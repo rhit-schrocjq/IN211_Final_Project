fa29c40a9d95a7d82f563255e7ba87390d57b5a3
rhit-mirandm1 <95196506+rhit-mirandm1@users.noreply.github.com>
2023-11-09 21:36:07 -0500

    ba

--- public/scripts/mainResources.js
+++ public/scripts/mainResources.js
@@ -1,5 +1,6 @@
 
 let id = ""
+
 main = function () {
   document.getElementById("first").onclick = (event) => {
     window.open('index.html')
@@ -22,6 +23,94 @@ main = function () {
     document.getElementById("changingTitle").innerHTML = countyName + " County"
   }
   loadData();
+  loadBar();
+}
+
+async function loadBar(){
+const taxCats = ["Education", "Basic Needs","Consumer Services","Criminal Justice and Legal Services","Environment and Public Health/Safety", "Health Care", "Income Support and Employment","Individual and Family Life",
+"Mental Health and Substance Use Disorder Services","Organizational/Community/International Services"];
+taxCats.forEach(function (taxonomy_category){
+  if (taxonomy_category == "Education") {
+    icon = 'school'
+    colorName = 'rgb(217, 131,26)';
+  } 
+  if (taxonomy_category == "Basic Needs"){
+    icon = 'home'
+    colorName = 'rgb(0, 0,0)';
+
+  }
+  if (taxonomy_category == "Consumer Services"){
+    icon = 'contract_edit'
+    colorName = 'rgb(74, 75,77)';
+
+  }
+  if (taxonomy_category == "Criminal Justice and Legal Services"){
+    icon = 'local_police'
+    colorName = 'rgb(0, 30,222)';
+
+  }
+  if (taxonomy_category == "Environment and Public Health/Safety"){
+    icon = 'source_environment'
+    colorName = 'rgb(11, 161,88)';
+
+  }
+  if (taxonomy_category == "Health Care"){
+    icon = 'health_and_safety'
+    colorName = 'rgb(161, 11,11)';
+
+  }
+  if (taxonomy_category == "Income Support and Employment"){
+    icon = 'payments'
+    colorName = 'rgb(114, 156,100)';
+
+  }
+  if (taxonomy_category == "Individual and Family Life"){
+    icon = 'diversity_1'
+    colorName = 'rgb(11, 93,161)';
+
+  }
+  if (taxonomy_category == "Mental Health and Substance Use Disorder Services"){
+    icon = 'stress_management'
+    colorName = 'rgb(51, 176,138)';
+
+  }
+  if (taxonomy_category == "Organizational/Community/International Services"){
+    icon = 'volunteer_activism'
+    colorName = 'rgb(166, 41, 155)';
+
+  }
+
+  const colIcon =   createAElement(['col-2'], `<span id="child" style="color:${colorName};" class="material-symbols-outlined">${icon}</span>`);
+  document.getElementById("menuBar").appendChild(colIcon);
+  colIcon.onclick = (event) => {
+    filterSelection(`${taxonomy_category}`);
+  }
+})
+ 
+  
+}
+async function filterSelection(taxonomy_category){
+    document.getElementById('all-cards').innerHTML="";
+    try {
+      const response = await fetch(`http://localhost:3030/resources/county/$}`);
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const data = await response.json();
+  
+      console.log(Object.keys(data).length);
+      const fragment = new DocumentFragment();
+  
+      for (let j = 0; j < Object.keys(data).length; j++) {
+  
+        fragment.appendChild(createCard(data[j]));
+      }
+  
+      document.getElementById('all-cards').appendChild(fragment);
+  
+    } catch (error) {
+      console.error('Error fetching resources:', error);
+    }
 }
 
 
@@ -40,6 +129,7 @@ async function loadData() {
 
       fragment.appendChild(createCard(data[j]));
     }
+
     document.getElementById('all-cards').appendChild(fragment);
 
   } catch (error) {
@@ -59,28 +149,109 @@ function createDivElement(classes, text) {
 
   return div;
 }
+function createAElement(classes, text) {
+  const a = document.createElement('a');
+
+  if (classes.length)
+    a.classList.add(...classes);
+
+  if (text)
+    a.innerHTML = text;
+
+  return a;
+}
+function iconChecker(item,row){
+  let icon = "";
+  let colorName = "";
+  const agencyName = item.agency_name.toLowerCase();
+
+
+  if (item.taxonomy_category == "Education") {
+    icon = 'school'
+    colorName = 'rgb(217, 131,26)';
+  } 
+  if (item.taxonomy_category == "Basic Needs"){
+    icon = 'home'
+    colorName = 'rgb(0, 0,0)';
+
+  }
+  if (item.taxonomy_category == "Consumer Services"){
+    icon = 'contract_edit'
+    colorName = 'rgb(74, 75,77)';
+
+  }
+  if (item.taxonomy_category == "Criminal Justice and Legal Services"){
+    icon = 'local_police'
+    colorName = 'rgb(0, 30,222)';
+
+  }
+  if (item.taxonomy_category == "Environment and Public Health/Safety"){
+    icon = 'source_environment'
+    colorName = 'rgb(11, 161,88)';
+
+  }
+  if (item.taxonomy_category == "Health Care"){
+    icon = 'health_and_safety'
+    colorName = 'rgb(161, 11,11)';
+
+  }
+  if (item.taxonomy_category == "Income Support and Employment"){
+    icon = 'payments'
+    colorName = 'rgb(114, 156,100)';
+
+  }
+  if (item.taxonomy_category == "Individual and Family Life"){
+    icon = 'diversity_1'
+    colorName = 'rgb(11, 93,161)';
+
+  }
+  if (item.taxonomy_category == "Mental Health and Substance Use Disorder Services"){
+    icon = 'stress_management'
+    colorName = 'rgb(51, 176,138)';
+
+  }
+  if (item.taxonomy_category == "Organizational/Community/International Services"){
+    icon = 'volunteer_activism'
+    colorName = 'rgb(166, 41, 155)';
+
+  }
+ 
+  
+
+  const colIcon = createDivElement(['col-1'], `<span style="color:${colorName};" class="material-symbols-outlined">${icon}</span>`);
+  return colIcon;
+   
+
+}
 function createCard(item) {
   const agencyName = item.agency_name.toLowerCase();
 
-  let icon = "";
-  if (item.taxonomy_category == "Basic Needs") {
-    icon = 'school'
-  } 
-
-
-  const colIcon = createDivElement(['col-1'], `<span class="material-symbols-outlined">${icon}</span>`);
-  const colTitle = createDivElement(['col-3'], agencyName);
-  const colType = createDivElement(['col-4'], item.taxonomy_name);
-  const colLink = createDivElement(['col-4'], `<a href=${item.service_website}>${item.service_website}</a>`);
-
   const row = createDivElement(['row']);
+  colIcon = iconChecker(item,row);
   row.appendChild(colIcon);
+  const colTitle = createDivElement(['col-4'], '<h4 class="fw-bold p-0 m-0 color-primary">'+ agencyName + '</h4>');
+  const colType = createDivElement(['col-3'], 'Type: '+ item.taxonomy_name);
+  const colContact = createDivElement(['col-4'], 'Contact: '+item.site_number);
+
+  const newRow = createDivElement(['row']);
+  const colLink = createDivElement(['col-7'], `<a href=${item.service_website}>${item.service_website}</a>`);
+  const addressRow = createDivElement(['row']);
+  const colAddress = createDivElement(['col-4'],'Address: '+item.address_1);
+
+ 
+ 
   row.appendChild(colTitle);
   row.appendChild(colType);
-  row.appendChild(colLink);
+  row.appendChild(colContact);
+
+  newRow.append(createDivElement(['col-1']));
+  newRow.appendChild(colLink);
+  newRow.appendChild(colAddress);
+  
 
   const cardBody = createDivElement(['card-body']);
   cardBody.appendChild(row);
+  cardBody.append(newRow);
 
 
   const card = createDivElement(['card']);
