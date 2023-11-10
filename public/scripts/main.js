90c02a8a1d945f87abd3b2c1592f182e5fa9a754
ROSE-HULMAN\schrocjq <schrocjq@rose-hulman.edu>
2023-11-09 21:35:30 -0500

    update!!

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -23,21 +23,70 @@ main = function () {
       window.open('resources.html?county=' + countyText.innerHTML)
     }
   })
-  const searchButton = document.getElementById('search-button');
-  const searchInput = document.getElementById('search-input');
+  const searchButton = document.getElementById('searchCountyButton');
+  const searchInput = document.getElementById('searchCountyInput');
+  const searchDropdown = document.getElementById('searchCountyDropdown');
+  // countySelectForm.onblur = (event) => {
+  //   console.log("hi")
+  //   searchDropdown.style.display = "none";
+  // }
+  searchInput.addEventListener('blur', function () {
+    setTimeout(function () {
+      // The currently focused element 
+      if (document.activeElement != searchDropdown && document.activeElement != searchButton) {
+        searchDropdown.style.display = "none";
+      }
+    }, 0);
+  });
+  searchDropdown.addEventListener('blur', function () {
+    setTimeout(function () {
+      // The currently focused element 
+      if (document.activeElement != searchInput && document.activeElement != searchButton) {
+        searchDropdown.style.display = "none";
+      }
+    }, 0);
+  });
+  searchButton.addEventListener('blur', function () {
+    setTimeout(function () {
+      // The currently focused element 
+      if (document.activeElement != searchDropdown && document.activeElement != searchInput) {
+        searchDropdown.style.display = "none";
+      }
+    }, 0);
+  });
+  searchButton.onclick = (event) => {
+    if (searchDropdown.options[searchDropdown.selectedIndex]) {
+      window.open('resources.html?county=' + searchDropdown.options[searchDropdown.selectedIndex].innerHTML);
+    }
+  }
   const host = window.location.host;
-  searchInput.addEventListener('input', async function() {
+  searchInput.addEventListener('input', async function () {
     const inputValue = searchInput.value;
-    if(inputValue) {
+    if (inputValue) {
       try {
         const response = await fetch(`http://${host}/counties/${inputValue}`);
         const data = await response.json();
-        console.log(data)
+        searchDropdown.style.display = "initial";
+        searchDropdown.innerHTML = "";
+        let index = 0;
+        data.forEach(county => {
+          newOption = document.createElement("option");
+          newOption.innerHTML = county;
+          newOption.onclick = (event) => {
+            window.open('resources.html?county=' + county)
+          }
+          if (index == 0) {
+            newOption.selected = "selected";
+          }
+          searchDropdown.appendChild(newOption);
+          index++;
+        })
       } catch (error) {
         console.log("Error fetching Data: ", error)
       }
     }
   });
+
 }
 
 document.addEventListener('DOMContentLoaded', (event) => {
